bbf9ec731abfbbe50baa406e2c144bc9b5ea7c4f
AaronHong1024 <93551579+AaronHong1024@users.noreply.github.com>
2025-08-27 13:43:51 -0400

    Add copyright and license information to version.hpp

--- include/version.hpp
+++ include/version.hpp
@@ -1,6 +1,8 @@
 //
 //  version.hpp.in
 //
+// Copyright (c) Boucher Lab. All rights reserved.
+// Licensed under the GNU license. See the LICENSE file in the repository root for full license information.
 
 #ifndef version_hpp
 #define version_hpp
