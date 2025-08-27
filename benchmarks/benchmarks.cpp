d65397694c471d70dd13287c1ddcdd1ca3e7957c
AaronHong1024 <93551579+AaronHong1024@users.noreply.github.com>
2025-08-27 13:44:54 -0400

    Add licensing comments to benchmarks.cpp

--- benchmarks/benchmarks.cpp
+++ benchmarks/benchmarks.cpp
@@ -2,6 +2,8 @@
 //  benchmarks.cpp
 //
 //
+// Copyright (c) Boucher Lab. All rights reserved.
+// Licensed under the GNU license. See the LICENSE file in the repository root for full license information.
 
 #include <random>
 
